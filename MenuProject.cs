035a5b1c37d7e895820d8891123123468a5938b6
HuiYing <huiyingkhoo14018@gmail.com>
2022-07-02 23:33:06 +0800

    Added getter and setter functions for MenuProject class.

--- MenuProject.cs
+++ MenuProject.cs
@@ -6,5 +6,15 @@ namespace ImgChecker
 {
     class MenuProject
     {
+        public string projectName { get; set; }
+
+        public string projectDescription { get; set; }
+
+        public string projectLocation { get; set; }
+
+        public string projectCreationDate { get; set; }
+
+        public string projectModifiedTime { get; set; }
+
     }
 }
